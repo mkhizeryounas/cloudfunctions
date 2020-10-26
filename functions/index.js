daf192e1559c5eaa255b176e359a916434ce1e0e
Khizer Younas <m.khizeryounas@gmail.com>
2020-10-26 14:24:11 +0500

    feat: env file added

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 const functions = require('firebase-functions');
 
 exports.status = functions.https.onRequest((request, response) => {
+  console.log('PROCESS', process.env.name);
   functions.logger.info('Hello logs!', { structuredData: true });
   response.json({
     message: 'Hello from Firebase!',
